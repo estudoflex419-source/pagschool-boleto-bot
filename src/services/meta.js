f20c7c6e9b63f33d5a8daef30f743cfbd10dd13d
estudoflex419-source <estudoflex419@gmail.com>
2026-03-16 12:55:23 -0300

    Update meta.js

--- src/services/meta.js
+++ src/services/meta.js
@@ -1,21 +1,37 @@
 const axios = require("axios")
-const {META_PHONE_ID,META_TOKEN} = require("../config")
+const { META_PHONE_ID, META_TOKEN } = require("../config")
 
-async function sendText(phone,text){
-  await axios.post(
-    `https://graph.facebook.com/v19.0/${META_PHONE_ID}/messages`,
-    {
-      messaging_product:"whatsapp",
-      to:phone,
-      type:"text",
-      text:{body:text}
-    },
-    {
-      headers:{
-        Authorization:`Bearer ${META_TOKEN}`
-      }
+async function sendText(phone, text) {
+  try {
+    const url = `https://graph.facebook.com/v19.0/${META_PHONE_ID}/messages`
+
+    const payload = {
+      messaging_product: "whatsapp",
+      to: String(phone),
+      type: "text",
+      text: { body: String(text) }
     }
-  )
+
+    const resp = await axios.post(url, payload, {
+      headers: {
+        Authorization: `Bearer ${META_TOKEN}`,
+        "Content-Type": "application/json"
+      },
+      validateStatus: () => true
+    })
+
+    console.log("[META SEND STATUS]", resp.status)
+    console.log("[META SEND DATA]", JSON.stringify(resp.data))
+
+    if (resp.status >= 400) {
+      throw new Error(`Meta erro ${resp.status}: ${JSON.stringify(resp.data)}`)
+    }
+
+    return resp.data
+  } catch (error) {
+    console.error("[META SEND ERROR]", error.message)
+    throw error
+  }
 }
 
-module.exports={sendText}
+module.exports = { sendText }
