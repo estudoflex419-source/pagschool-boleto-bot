e4b6213dd65ba4430c9eaa4cc4dd892046a42c99
estudoflex419-source <estudoflex419@gmail.com>
2026-03-16 14:37:17 -0300

    Update conversations.js

--- src/crm/conversations.js
+++ src/crm/conversations.js
@@ -1,17 +1,35 @@
 const conversations = new Map()
 
-function getConversation(phone){
-  if(!conversations.has(phone)){
-    conversations.set(phone,{
-      step:"menu",
-      name:"",
-      cpf:"",
-      course:"",
-      payment:""
+function getConversation(phone) {
+  if (!conversations.has(phone)) {
+    conversations.set(phone, {
+      step: "menu",
+      path: "",
+      course: "",
+      goal: "",
+      experience: "",
+      payment: "",
+      name: "",
+      cpf: "",
+      birthDate: "",
+      gender: "",
+      phone: "",
+      cep: "",
+      street: "",
+      number: "",
+      complement: "",
+      neighborhood: "",
+      city: "",
+      state: "",
+      dueDay: "",
+      alunoId: null,
+      contratoId: null,
+      parcelaId: null,
+      nossoNumero: ""
     })
   }
 
   return conversations.get(phone)
 }
 
-module.exports = {getConversation}
+module.exports = { getConversation }
