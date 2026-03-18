6d277dc0ecc1585b4986f4c85ba1a296ac4d428b
estudoflex419-source <estudoflex419@gmail.com>
2026-03-18 18:46:31 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 function readEnv(...keys) {
   for (const key of keys) {
     const value = process.env[key]
@@ -14,19 +16,22 @@ module.exports = {
   PUBLIC_BASE_URL: readEnv("PUBLIC_BASE_URL").replace(/\/$/, ""),
 
   META_GRAPH_VERSION: readEnv("META_GRAPH_VERSION") || "v22.0",
-  META_TOKEN: readEnv("META_ACCESS_TOKEN", "META_TOKEN"),
-  META_PHONE_ID: readEnv("META_PHONE_NUMBER_ID", "META_PHONE_ID"),
+  META_TOKEN: readEnv("META_TOKEN"),
+  META_PHONE_ID: readEnv("META_PHONE_ID"),
   META_VERIFY_TOKEN: readEnv("META_VERIFY_TOKEN"),
 
-  OPENAI_KEY: readEnv("OPENAI_API_KEY"),
+  OPENAI_KEY: readEnv("OPENAI_API_KEY", "OPENAI_KEY"),
   OPENAI_MODEL: readEnv("OPENAI_MODEL") || "gpt-4.1-mini",
 
   PAGSCHOOL_BASE_URL:
     readEnv("PAGSCHOOL_BASE_URL", "PAGSCHOOL_ENDPOINT") ||
     "https://sistema.pagschool.com.br/prod/api",
 
-  PAGSCHOOL_EMAIL: readEnv("PAGSCHOOL_EMAIL"),
+  PAGSCHOOL_EMAIL: readEnv("PAGSCHOOL_EMAIL", "PAGSCHOOL_USER", "PAGSCHOOL_USERNAME"),
   PAGSCHOOL_PASSWORD: readEnv("PAGSCHOOL_PASSWORD"),
+  PAGSCHOOL_CODIGO_ESCOLA: readEnv("PAGSCHOOL_CODIGO_ESCOLA", "CODIGO_ESCOLA"),
+  PAGSCHOOL_AUTH_SCHEME: readEnv("PAGSCHOOL_AUTH_SCHEME"),
+  INTERNAL_LEAD_NOTIFY_PHONE: readEnv("INTERNAL_LEAD_NOTIFY_PHONE"),
 
   PAGSCHOOL_URL:
     readEnv("PAGSCHOOL_BASE_URL", "PAGSCHOOL_ENDPOINT") ||
